4498b2e4e14f9d57c5e4ff26b87d7bbf79b77220
Matheus Guilherme <mgsmmanhaes0209@gmail.com>
2023-03-10 17:17:43 -0300

    commit

--- aula012-ex/scripts/script.js
+++ aula012-ex/scripts/script.js
@@ -1,1 +1,21 @@
-alert('Olá')
+function carregar()
+{
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12)
+    {
+        img.src = "imagens/manha-redondo2.png"
+    }
+    else if (hora >= 12 && hora < 18)
+    {
+        img.src = "imagens/tarde-redondo2.png"
+    }
+    else
+    {
+        img.src = "imagens/noite-redondo.png"
+    }
+}
+
