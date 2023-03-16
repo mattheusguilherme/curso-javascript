32771a829464bef814fd61b5e74fd5adf8ffde34
Matheus Guilherme <mgsmmanhaes0209@gmail.com>
2023-03-16 17:35:04 -0300

    Update script.js

--- aula013/scripts/script.js
+++ aula013/scripts/script.js
@@ -2,9 +2,9 @@ function verificar()
 {
     var data = new Date()
     var ano = data.getFullYear()
-    var fano = document.getElementById("iano")
-    var res = document.querySelector("div#res")
-    if (fano.ariaValueMax.length == 0 || Number(fano.value) > ano) 
+    var fano = document.getElementById('iano')
+    var res = document.querySelector('div#res')
+    if (fano.value.length == 0 || Number(fano.value) > ano) 
     {
         window.alert('[ERRO] verifique os dados e tente novamente!')
     }
@@ -13,10 +13,43 @@ function verificar()
         var fsex = document.getElementsByName('isex')
         var idade = ano - Number(fano.value)
         var genero = ''
-
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
         if (fsex[0].checked)
         {
-            
+            genero = 'Homem'
+            if (idade >=0 && idade < 10)
+            {
+                img.setAttribute('src', 'imagens/foto-bebe-m.png')
+            } else if (idade < 21)
+            {
+                img.setAttribute('src', 'imagens/foto-jovem-m.png')
+            } else if (idade < 60)
+            {
+                img.setAttribute('src', 'imagens/foto-adulto-m.png')
+            } else
+            {
+                img.setAttribute('src', 'imagens/foto-idoso-m.png')
+            }
+        } else if (fsex[1].checked)
+        {
+            genero = 'Mulher'
+            if (idade >=0 && idade < 10)
+            {
+                img.setAttribute('src', 'imagens/foto-bebe-f.png')
+            } else if (idade < 21)
+            {
+                img.setAttribute('src', 'imagens/foto-jovem-f.png')
+            } else if (idade < 60)
+            {
+                img.setAttribute('src', 'imagens/foto-adulto-f.png')
+            } else
+            {
+                img.setAttribute('src', 'imagens/foto-idoso-f.png')
+            }
         }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
     }
 }
